21ddad844e01be505d835ed425e82bb7202a6194
Stonevast <59499554+Stonevast@users.noreply.github.com>
2021-01-06 16:27:07 +1100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    const doodler = document.createElement('div')
+
+    function createDoodler() {
+        grid.appendChild(doodler)
+        doodler.classList.add('doodler')
+        doodler.style.left 
+
+    }
+    createDoodler()
+})
